699dbef2d7bdd07e25b87ff55293d4b347a517ec
Danny Díaz <meliodasfistery@gmail.com>
2021-06-23 23:22:33 -0500

    Refactoring project name

--- django-services/urls.py
+++ django-services/urls.py
@@ -1,4 +1,4 @@
-"""club_ia URL Configuration
+"""django-services URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- django-services/wsgi.py
+++ django-services/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for club_ia project.
+WSGI config for django-services project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_ia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-services.settings')
 
 application = get_wsgi_application()
